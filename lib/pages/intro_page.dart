a3b8b5d45694b1d694e0fa6adc0c083c03b19b70
eloimoncho <eloi.moncho@estudiantat.upc.edu>
2024-02-13 13:10:46 +0100

    Step 1: Simple Form

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatefulWidget {
+  const IntroPage({super.key});
+
+  @override
+  State<IntroPage> createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  bool isConnected = false;
+  String titleButton = 'Conectar';
+  Color colorButton = const Color.fromARGB(255, 44, 33, 243);
+  
+  void changeConnected(){
+    isConnected = !isConnected;
+    setState(() {
+      if(isConnected == false){
+      titleButton = 'Conectar';
+      colorButton = const Color.fromARGB(255, 44, 33, 243);
+    }
+    else{
+      titleButton = 'Desconectar';
+      colorButton = Color.fromARGB(212, 170, 0, 227);
+    }
+    });
+    
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tutorial Flutter', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.cyan,
+        
+        ),
+        body: Center(
+          child: InkWell(
+            onTap: changeConnected,
+            child: Container(
+              height: 40,
+              width: 100,
+              decoration: BoxDecoration(
+                color: colorButton,
+                borderRadius: const BorderRadius.all(Radius.circular(120)),),
+              child: Center(child: Text(titleButton, style: const TextStyle(color: Colors.white),)), 
+          ),
+          )
+        )
+    );
+  }
+}
